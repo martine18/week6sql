11e4ee82fb6cc4dd7a219380f7576d04ac54a23e
martine18 <the7sevenstar@gmail.com>
2023-02-22 03:30:42 -0800

    ex update code

--- w6day1/ex1.sql
+++ w6day1/ex1.sql
@@ -1,1 +1,7 @@
-SELECT * FROM actors;
+CREATE TABLE actors(
+ actor_id SERIAL PRIMARY KEY,
+ first_name VARCHAR (50) NOT NULL,
+ last_name VARCHAR (100) NOT NULL,
+ age DATE NOT NULL,
+ number_oscars SMALLINT NOT NULL
+)
